9e25ee42adf6d4e2a6308dd1040d5f5033cd303a
Zhenming Wu <simonuit.fox@gmail.com>
2025-03-24 13:07:31 +0100

    run_job.sh

--- run_job.sh
+++ run_job.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 #SBATCH -p accel
 #SBATCH --account=nn10004k
-#SBATCH --output=ouput_adamW_focalloss_4GPU.txt
+#SBATCH --output=output_adamW_OCRNet_4GPU_80000.txt
 #SBATCH --error=error.txt
 #SBATCH --job-name=sam
 #SBATCH --gres=gpu:p100:4
 #SBATCH --ntasks=4
 #SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=6
 #SBATCH --mem-per-cpu=5G
 #SBATCH --time=15:15:00
 
-export PYTHONPATH="$(dirname $0)/..":$PYTHONPATH
-srun python -u tools/train.py data/sam_vit-b_OCRNet_SeaObject-1024x1024_MultiGPU.py --launcher=slurm
+## Set up job environment:
+set -o errexit  # Exit the script on any error
+
+set -o nounset  # Treat any unset variables as an error
+
+# module --quiet purge  # Reset the modules to the system default
+module purge --force
+source /cluster/projects/nn10004k/packages_install/torch_cu121/bin/activate
+module load  CUDA/12.1.1 
+
+# export PYTHONPATH="$(dirname $0)/..":$PYTHONPATH
+srun python -u tools/train.py data/sam_vit-b_OCRNet_SeaObject-1024x1024_MultiGPU.py  \
+    --work-dir /cluster/projects/nn10004k/packages_install/seaobject_ocrnet5000  \
+    # --launcher=slurm
